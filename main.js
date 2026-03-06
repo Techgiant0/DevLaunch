ebd131f919ed9c416ab5c76f85b722dbc75ca406
Techgiant <simonsamuel195@gmail.com>
2026-03-06 21:37:23 +0100

    added delete icon

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const popup = document.getElementById("error-popup");
 const closeBtn = document.getElementById("close-btn");
 const contentContainer = document.getElementById("content-container");
 const emptyTask = document.getElementsByClassName("empty-task");
+const deleteBtn = document.getElementById('delete')
 const tasks = [];
 
 filterButtons.forEach((button) => {
@@ -51,10 +52,10 @@ taskInput.addEventListener("keypress", (e) => {
     Addtasks();
   }
 });
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const savedTasks = localStorage.getItem("usersTask");
   if (savedTasks) {
-    allTasks.innerHTML=''
+    allTasks.innerHTML = "";
     const parsedTasks = JSON.parse(savedTasks);
     tasks.push(...parsedTasks);
     renderTasks();
@@ -80,9 +81,9 @@ function Addtasks() {
     void popup.offsetWidth;
     popup.classList.add("animate-popup");
   } else {
-    createTasks()
-    saveTasks()
-    renderTasks()
+    createTasks();
+    saveTasks();
+    renderTasks();
   }
 }
 
@@ -104,10 +105,25 @@ function renderTasks() {
   const savedTasks = localStorage.getItem("usersTask");
   const parsedTask = JSON.parse(savedTasks);
   parsedTask.forEach((currentTask) => {
-    const eachTaskContainer = `<label class="checkbox-container">
-                            <input type="checkbox" />
-                            <span>${currentTask.taskName}</span>
-                        </label>`;
+    const eachTaskContainer = ` <div class="checkbox-container">
+              <div class="check-item">
+                <input type="checkbox" />
+                <span>${currentTask.taskName}</span>
+              </div>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                class="bi bi-trash3-fill"
+                viewBox="0 0 16 16"
+                id="delete"
+              >
+                <path
+                  d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"
+                />
+              </svg>
+            </div>b`;
     allTasks.insertAdjacentHTML("beforeend", eachTaskContainer);
   });
 }
